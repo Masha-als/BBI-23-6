fbf0c314abfffda33014d6756e3fa537eab23bde
marials_s <79412013+Masha-als@users.noreply.github.com>
2024-03-29 19:50:42 +0300

    7 ЛР

---
+++ lab7_task1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+/*
+1) Составить программу для обработки результатов кросса на 500 м для женщин.  
+Для каждой участницы ввести фамилию, группу, фамилию преподавателя,
+результат. Получить результирующую таблицу, упорядоченную по результатам, в  
+которой содержится также информация о выполнении норматива. Определить  
+суммарное количество участниц, выполнивших норматив.
+
+2.	Сделать абстрактный класс, и от него создать 2-х наследников: бег на 100м и 500м. 
+Провести соревнования по забегу на эти 2 дистанции и вывести 2 таблицы с результатами.
+*/
+
+
+namespace lab_7
+{
+    abstract class Race
+    {
+        public struct Participant
+        {
+            private string surname;
+            private int group;
+            private string surnameCoach;
+            private int result;
+            private int norm;
+
+            public Participant(string surname, int group, string surnameCoach, int result, int norm)
+            {
+                this.surname = surname;
+                this.group = group;
+                this.surnameCoach = surnameCoach;
+                this.result = result;
+                this.norm = norm;
+            }
+
+            public int Result { get { return result; } }
+            public bool Passed { get { return result <= norm; } }
+
+            public void Print()
+            {
+                string status = Passed ? "Норматив выполнен" : "Норматив не выполнен";
+                Console.WriteLine($"{surname} {group} {surnameCoach} {result} - {status}");
+            }
+        }
+
+        public abstract Participant[] StartRace();
+
+        public void RunRace()
+        {
+            Participant[] participants = StartRace();
+
+            Array.Sort(participants, (a, b) => a.Result.CompareTo(b.Result));
+
+            Console.WriteLine();
+            foreach (var participant in participants)
+            {
+                participant.Print();
+            }
+
+            int count = 0;
+            foreach (var participant in participants)
+            {
+                if (participant.Passed)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Выполнило норматив: " + count);
+        }
+    }
+
+    class Race100m : Race
+    {
+        public override Participant[] StartRace()
+        {
+            Participant[] participants = new Participant[4];
+
+            participants[0] = new Participant("Иванов", 1, "Новиков", 10, 11);
+            participants[1] = new Participant("Смирнов", 2, "Лебедев", 12, 11);
+            participants[2] = new Participant("Петров", 1, "Новиков", 11, 11);
+            participants[3] = new Participant("Соколов", 3, "Волков", 9, 11);
+        
+            return participants;
+        }
+    }
+
+    class Race500m : Race
+    {
+        public override Participant[] StartRace()
+        {
+            Random random = new Random();
+            Participant[] participants = new Participant[3];
+
+            participants[0] = new Participant("Кузнецов", 4, "Иванов", random.Next(60, 100), 75);
+            participants[1] = new Participant("Попов", 5, "Соколов", random.Next(60, 100), 75);
+            participants[2] = new Participant("Морозов", 4, "Иванов", random.Next(60, 100), 75);
+
+            return participants;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Race100m race100m = new Race100m();
+            Race500m race500m = new Race500m();
+
+            Console.WriteLine("Забег на 100м:");
+            race100m.RunRace();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Забег на 500м:");
+            race500m.RunRace();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab7_task3.cs
@@ -0,0 +1,112 @@
+﻿using System;
+//using System.Diagnostics.Metrics;
+/*
+3)Соревнования по футболу между командами проводятся в два этапа. Для  проведения первого этапа участники разбиваются на две группы по 12 команд.  
+Для проведения второго этапа выбирается 6 лучших команд каждой группы по  результатам первого этапа. Составить список команд участников второго этапа.
+2. Создать класс «Футбольная команда» с 2 классами-наследниками: «Женская футбольная команда» и «Мужская футбольная команда». 
+Провести среди них отдельные соревнования, но вывести в общей таблице по 6 лучших женских и мужских команд, отсортированных по общему количеству баллов с указанием пола (т.е.: 1. ЦСКА женская команда 13 баллов; 2 Динамо мужская команда 12 баллов; 3 Спартак мужская команда 10 баллов…). 
+Использовать динамическую связку: преобразование классов.
+*/
+
+namespace lab_7_task_3
+{
+    class Team
+    {
+        protected string name;
+        protected int count = 0;
+        public string Name { get { return name; } }
+        public int Count { get { return count; } }
+        public Team(string name)
+        {
+            this.name = name;
+        }
+        public void Win()
+        {
+            count++;
+        }
+        public void Print()
+        {
+            Console.WriteLine(name + "  count:" + count);
+        }
+    }
+    class FemaleTeam : Team
+    {
+        public FemaleTeam(string name) : base("female " + name) { }
+    }
+    class MaleTeam : Team
+    {
+        public MaleTeam(string name) : base("male " + name) { }
+    }
+    class Program
+    {
+        static void Matches(Team[] teams)
+        {
+            Random rand = new Random();
+            int a; int b;
+            for (int i = 0; i < teams.Length - 1; i++)
+            {
+                for (int j = i + 1; j < teams.Length; j++)
+                {
+                    //Console.WriteLine(teams[i].Name + " - " + teams[j].Name);
+                    a = rand.Next(0, 10); b = rand.Next(0, 10);
+                    if (a > b)
+                    {
+                        teams[i].Win(); //Console.WriteLine("Winner - " + teams[i].Name); }
+                        if (a < b)
+                        {
+                            teams[j].Win(); //Console.WriteLine("Winner - " + teams[j].Name); }
+                                            //Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+        static void SortTeams(Team[] teams)
+        {
+            Team val;
+            for (int i = 0; i < teams.Length; i++)
+            {
+                val = teams[i];
+                for (int j = i - 1; j >= 0;)
+                {
+                    if (val.Count > teams[j].Count)
+                    {
+                        teams[j + 1] = teams[j];
+                        j--;
+                        teams[j + 1] = val;
+                    }
+                    else { break; }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            FemaleTeam[] femaleteams = new FemaleTeam[12];
+            MaleTeam[] maleteams = new MaleTeam[12];
+            for (int i = 0; i < femaleteams.Length; i++)
+            {
+                femaleteams[i] = new FemaleTeam("team_" + i);
+                maleteams[i] = new MaleTeam("team_" + i);
+            }
+            Matches(maleteams);
+            SortTeams(maleteams);
+            Matches(femaleteams);
+            SortTeams(femaleteams);
+            Team[] generalteams = new Team[12];
+            for (int i = 0; i < 6; i++)
+            {
+                generalteams[i] = maleteams[i];
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                generalteams[i + 6] = femaleteams[i];
+            }
+            SortTeams(generalteams);
+            for (int i = 0; i < generalteams.Length; i++)
+            {
+                generalteams[i].Print();
+            }
+            Console.ReadKey();
+        }
+    }
+}
